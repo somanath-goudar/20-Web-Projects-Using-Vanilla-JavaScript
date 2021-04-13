d56b57836afbaa041934f26f33b2549f0adef040
Somanath Goudar <somugowda8050@gmail.com>
2021-04-13 16:46:05 +0530

    project 2 completed

--- 2-movie-seat-booking/script.js
+++ 2-movie-seat-booking/script.js
@@ -3,6 +3,8 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI();
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -29,6 +31,24 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// get data from localstorage and populate ui
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
@@ -44,3 +64,6 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 });
+
+// intial count and total
+updateSelectedCount();
