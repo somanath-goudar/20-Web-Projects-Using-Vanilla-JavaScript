22e2852ed235db9615663362776867be8ae7bb45
Somanath Goudar <somugowda8050@gmail.com>
2021-04-14 12:15:29 +0530

    play pause stop

---
+++ 3-custom-video-player/script.js
@@ -0,0 +1,50 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+//set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+//stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
